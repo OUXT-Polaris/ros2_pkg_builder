ad80a497efd6c0761f5b2274fa2db67a7a4870d4
Masaya Kataoka <ms.kataoka@gmail.com>
2023-08-16 16:33:19 +0900

    update Dockerfile
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- docker/base/run.sh
+++ docker/base/run.sh
@@ -7,4 +7,4 @@ fi
 echo "" > $(pwd)/amd64/rosdep/humble.yaml
 cp update_apt_repo.sh $(pwd)/amd64/update_apt_repo.sh
 docker build -t build_packages .
-docker run -it --rm --mount type=bind,source="$(pwd)"/amd64,target=/artifacts build_packages
+docker run -it --rm --mount type=bind,source="$(pwd)"/amd64,target=/artifacts wamvtan/ros2_pkg_builder:humble
