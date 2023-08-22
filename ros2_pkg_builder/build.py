04602549d0e1a7da48276a4112a57f79702c9112
Masaya Kataoka <ms.kataoka@gmail.com>
2023-08-23 01:06:55 +0900

    enable bake images locally
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- ros2_pkg_builder/build.py
+++ ros2_pkg_builder/build.py
@@ -8,7 +8,9 @@ from python_on_whales import docker
 import ros2_pkg_builder
 
 
-def build_deb_packages(architecture: str, rosdistro: str, repos_file: str):
+def build_deb_packages(
+    architecture: str, rosdistro: str, repos_file: str, build_builder_image: bool
+) -> None:
     output_directory = Path(architecture).joinpath("rosdep")
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -19,6 +21,20 @@ def build_deb_packages(architecture: str, rosdistro: str, repos_file: str):
         Path(architecture).joinpath("update_apt_repo.sh"),
     )
     shutil.copyfile(repos_file, Path(architecture).joinpath("workspace.repos"))
+    if build_builder_image:
+        docker.buildx.bake(
+            [rosdistro],
+            load=True,
+            set={
+                "*.platform": "linux/" + architecture,
+                "*.context": Path(ros2_pkg_builder.__path__[0]).joinpath("docker"),
+            },
+            files=[
+                Path(ros2_pkg_builder.__path__[0])
+                .joinpath("docker")
+                .joinpath("docker-bake.hcl")
+            ],
+        )
     docker.run(
         image="wamvtan/ros2_pkg_builder:" + rosdistro,
         volumes=[(Path(architecture).absolute(), "/artifacts")],
@@ -31,16 +47,31 @@ def build_deb_packages(architecture: str, rosdistro: str, repos_file: str):
 
 def main():
     parser = argparse.ArgumentParser(description="Building debian packages")
-    parser.add_argument("architecture", choices=["amd64", "arm64"])
-    parser.add_argument("rosdistro", choices=["rolling", "iron", "humble"])
+    parser.add_argument(
+        "architecture",
+        choices=["amd64", "arm64"],
+        help="Target CPU architecture.",
+    )
+    parser.add_argument(
+        "rosdistro",
+        choices=["rolling", "iron", "humble"],
+        help="Target ROS 2 distribution.",
+    )
     parser.add_argument(
         "--repos",
         help="repos file for your workspace",
         type=str,
         default="workspace.repos",
     )
+    parser.add_argument(
+        "--build_builder_image",
+        action="store_true",
+        help="If true, build builder images in your local machine.",
+    )
     args = parser.parse_args()
-    build_deb_packages(args.architecture, args.rosdistro, args.repos)
+    build_deb_packages(
+        args.architecture, args.rosdistro, args.repos, args.build_builder_image
+    )
 
 
 if __name__ == "__main__":
